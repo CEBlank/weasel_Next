84a26820d7ca472fb84924d8c993376acf882a7a
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-18 21:29:05 -0600

    Auth0 setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,39 @@
-import Image from "next/image";
-//import config from '../mongoConfig.json';
-import mongodb from 'mongodb';
+import { auth0 } from "./lib/auth0";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogOutButton";
+import Profile from "./components/Profile";
 
 export default async function Home() {
-
-//const { MongoClient, ServerApiVersion } = require("mongodb");
-
-//mongodb.MongoClient;
-
-// Replace the placeholders in the connection string uri with your credentials
-
-const uri = "mongodb+srv://Vercel-Admin-weasel-games-db:"+process.env.DB_PASS+"@weasel-games-db.1iwb3zs.mongodb.net/?appName=weasel-games-db";
-
-const MongoClient = mongodb.MongoClient;
-const ServerApiVersion = mongodb.ServerApiVersion;
-
-// Create a client with options to specify Stable API Version 1
-const client = new MongoClient(uri,
-    {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-  ); 
-
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-
+  const session = await auth0.getSession();
+  const user = session?.user;
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hello World</h1>
-
-
-      </main>
+    <div className="app-container">
+      <div className="main-card-wrapper">
+        <img
+          src="https://cdn.auth0.com/quantum-assets/dist/latest/logos/auth0/auth0-lockup-en-ondark.png"
+          alt="Auth0 Logo"
+          className="auth0-logo"
+        />
+        <h1 className="main-title">Next.js + Auth0</h1>
+        
+        <div className="action-card">
+          {user ? (
+            <div className="logged-in-section">
+              <p className="logged-in-message">✅ Successfully logged in!</p>
+              <Profile />
+              <LogoutButton />
+            </div>
+          ) : (
+            <>
+              <p className="action-text">
+                Welcome! Please log in to access your protected content.
+              </p>
+              <LoginButton />
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
