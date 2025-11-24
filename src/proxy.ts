3bacb8842af4ea104b624f1fe5bdc81f386ce614
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-24 00:40:27 -0600

    ate my hero image
    this is fine

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,30 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { auth0 } from "./app/lib/auth0";
 
 export async function proxy(request: NextRequest) {
-  return await auth0.middleware(request);
+  const authRes = await auth0.middleware(request);
+
+    // authentication routes — let the middleware handle it
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+        return authRes;
+    }
+
+    // public routes — no need to check for session
+    if (request.nextUrl.pathname === ("/")) {
+        return authRes;
+    }
+
+    const { origin } = new URL(request.url)
+    const session = await auth0.getSession()
+
+    // user does not have a session — redirect to login
+    if (!session) {
+        return NextResponse.redirect(`${origin}/auth/login`)
+    }
+
+    return authRes
 }
+
 
 export const config = {
   matcher: [
