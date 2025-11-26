d25722b34aa9c1d0ca48bf2e26ade7516737d10a
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-26 00:28:04 -0600

    Update auth0.ts

--- src/app/lib/auth0.ts
+++ src/app/lib/auth0.ts
@@ -1,15 +1,17 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_CLIENT_SECRET, APP_BASE_URL, AUTH0_SECRET } from "./auth0.json"
+
 
 // Initialize the Auth0 client 
 export const auth0 = new Auth0Client({
   // Options are loaded from environment variables by default
   // Ensure necessary environment variables are properly set
-    domain: process.env.AUTH0_DOMAIN,
-    clientId: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    appBaseUrl: process.env.APP_BASE_URL,
-    secret: process.env.AUTH0_SECRET,
-    
+    domain: AUTH0_DOMAIN,
+    clientId: AUTH0_CLIENT_ID,
+    clientSecret: AUTH0_CLIENT_SECRET,
+    appBaseUrl: APP_BASE_URL,
+    secret: AUTH0_SECRET,
+
   authorizationParameters: {
     // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables are no longer automatically picked up by the SDK.
     // Instead, we need to provide the values explicitly.
