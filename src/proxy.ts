aa2a2f60024f2e866dc3f2cf3b17e146ad352aba
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-28 15:14:39 -0600

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,7 @@
 import type { NextRequest } from "next/server";
-//import { NextResponse } from "next/server";
 import { auth0 } from "./app/lib/auth0";
 
-
-export async function proxy(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   return await auth0.middleware(request);
 }
 
