bd61854d7622ba9e85b3241959d9606ed9d72471
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-30 17:57:35 -0600

    Update page.tsx

--- src/app/pages/account/page.tsx
+++ src/app/pages/account/page.tsx
@@ -1,5 +1,22 @@
 "use client"
 
+import { NextResponse, NextRequest } from 'next/server'
+import { auth, clerkClient } from '@clerk/nextjs/server'
+
+/* export async function POST(req: NextRequest) {
+  const { userId } = await auth()
+
+  if (!userId) return NextResponse.redirect(new URL('/sign-in', req.url))
+
+  const params = { firstName: 'John', lastName: 'Wick' }
+
+  const client = await clerkClient()
+
+  const user = await client.users.updateUser(userId, params)
+
+  return NextResponse.json({ user })
+} */
+
 const Account = () => {
   
 
