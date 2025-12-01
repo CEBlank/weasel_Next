3b38c4090b573a17b15d4a0d3fc9f5ecc0143fcd
Chris <96022511+CEBlank@users.noreply.github.com>
2025-11-30 19:19:36 -0600

    Update userAuth.tsx
    apparently it didn't merge

--- src/app/components/userAuth.tsx
+++ src/app/components/userAuth.tsx
@@ -1,32 +1,49 @@
 "use client"
 
-import { useUser } from "@auth0/nextjs-auth0";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 
-import { auth0 } from "../lib/auth0"
-import { redirect } from "next/navigation";
-
-import LoginButton from "./LoginButton";
-import LogoutButton from "./LogOutButton";
+//import { useUser } from "@auth0/nextjs-auth0";
+//import { auth0 } from "../lib/auth0";
+//import { redirect } from "next/navigation";
+//import LoginButton from "./LoginButton";
+//import LogoutButton from "./LogOutButton";
 //import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default function authUser() {
-  const { user } = useUser();
+
+
+//const clerkPublicKey = "pk_live_Y2xlcmsud2Vhc2VsZ2FtZXMuYXBwJA"
+
+//  const { user } = useUser();
 
 //  console.log("user everything: ", user);
   
   return (
-        
-    user ? (
-          <div id="profileRoute"
-            className="flex">  
-              <LogoutButton />
-          </div>
-    ) : 
-      <div>
-      <LoginButton />
-      </div>
-          
+  <>   
+      <ClerkProvider>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <a className="text-1xl">
+              Sign Up
+            </a>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </ClerkProvider>
+  </>
   );
 };
 
