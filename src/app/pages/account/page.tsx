f8fb2dc75e1065b085b1d101c768bdf1cc84dc71
Chris <96022511+CEBlank@users.noreply.github.com>
2025-12-06 18:44:15 -0600

    Update page.tsx

--- src/app/pages/account/page.tsx
+++ src/app/pages/account/page.tsx
@@ -6,41 +6,16 @@ import { UserProfile } from "@clerk/nextjs"
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-/* const DotIcon = () => {
+export default function Account() {
+
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
-      <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
-    </svg>
+    <>
+    <h2>
+      Account Page
+    </h2>
+    </>
   )
 }
-
-const CustomPage = () => {
-  return (
-    <div>
-      <h1>Custom page</h1>
-      <p>This is the content of the custom page.</p>
-    </div>
-  )
-} */
-
-/* const UserProfilePage = () => (
-  <UserProfile path="/user-profile" routing="path"> */
-    {/* You can pass the content as a component */}
-    {/* <UserProfile.Page label="Custom Page" labelIcon={<DotIcon />} url="custom-page">
-      <CustomPage />
-    </UserProfile.Page> */}
-
-    {/* You can also pass the content as direct children */}
-    {/* <UserProfile.Page label="Terms" labelIcon={<DotIcon />} url="terms">
-      <div>
-        <h1>Custom Terms Page</h1>
-        <p>This is the content of the custom terms page.</p>
-      </div>
-    </UserProfile.Page>
-  </UserProfile>
-)
-
-export default UserProfilePage */}
 
 
 
