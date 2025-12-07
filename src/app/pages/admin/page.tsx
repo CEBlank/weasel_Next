9a1af2fdea65ef50d57d7a26a2f0f66e7c95987f
Chris <96022511+CEBlank@users.noreply.github.com>
2025-12-07 17:32:52 -0600

    Update page.tsx

--- src/app/pages/admin/page.tsx
+++ src/app/pages/admin/page.tsx
@@ -2,9 +2,12 @@ import type { PropsWithChildren } from 'react'
 import { Protect } from '@clerk/nextjs'
 
 export default function Admin() {
+
+
+
   return (
     <Protect
-      feature='org:admin'
+      role='org:admin'
       fallback={<p>Only an Admin or Billing Manager can access this content.</p>}
     >
       <p>The settings page.</p>
