12b5281a690428d7aa452fe8f8078c66e8c6e706
Chris <96022511+CEBlank@users.noreply.github.com>
2025-12-13 01:10:08 -0600

    dependecies

---
+++ src/app/lib/dynamoDB.ts
@@ -0,0 +1,10 @@
+//probably don't need this, but easier to find here. 
+
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
